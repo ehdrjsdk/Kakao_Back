c5d0446893ca297749e3fc9f20320adecbf4d7af
Lixmna <kang7145471@naver.com>
2021-01-27 03:16:33 +0900

    혹시 이렇게 하면 안되나?

--- login_server.js
+++ login_server.js
@@ -1,8 +1,16 @@
-/*
-@author jae kyeong <kang7145471@naver.com>
-@version 0.0.1
-@file 카카오톡 클론코딩 메인 파일입니다.
+/** 
+ * @author jae kyeong <kang7145471@naver.com>
+ * @version 0.0.1
+ * @file 카카오톡 클론코딩 로그인 메인 파일입니다.
 */
+/** @constant app {express} node.js express를 사용하기 위해 필요한 기초적인 앱 상수입니다. */
+/** @constant users {Router} 로그인과 관련된 라우팅 시스템 연결 위치 상수입니다. */
+/** @constant cookieParser {function} 쿠키를 사용하고 쿠키안에서 어떤일이 일어나는지에 대해 알기위해만든 상수입니다. */
+/** 
+ * @constant db {string} Mongo DB에 연결하기 위해 만든 상수입니다. 
+ * @throws Mongo DB에 연결하지 못한다면 콘솔에 로그를 리턴힙니다.
+*/
+
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
--- routes/login_router/auth.js
+++ routes/login_router/auth.js
@@ -1,3 +1,9 @@
+/**
+ * 로그인된 사용자의 jwt를 비교분석하여 맞다면 사용자에 대한 정보를 json형식으로 되돌려줄 수있도록 하는 함수
+ * @class
+ * @param {*} req 
+ * @param {*} res 
+ */
 
 function auth(req, res)
 {
