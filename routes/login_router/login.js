f2df8c4ba468ee3a3f54a4560e2a016fe27ecced
Lixmna <kang7145471@naver.com>
2021-01-27 04:01:43 +0900

    안되는구마잉

--- routes/login_router/login.js
+++ routes/login_router/login.js
@@ -4,64 +4,61 @@ const bcrypt = require('bcryptjs');
 const User = require('../../models/User');
 const keys = require('../../config/keys');
 
-class login_Manager{
+function login(req, res)
+{
+    // @author <이름>
+    const email = req.body.email;
+    const password = req.body.password;
 
-    login(req, res)
-    {
-        // @author <이름>
-        const email = req.body.email;
-        const password = req.body.password;
-    
-        // email로 회원 찾기
-        User.findOne({email})
-            .then(user => {
-                if(!user){
-                    errors.email = "해당하는 회원이 존재하지 않습니다.";
-                    return res.status(400).json(errors);
-                }
-                bcryptchecker(password, user, res);
-            });
-    }
-
-    bcryptchecker(password, user, res)
-    {
-        bcrypt.compare(password, user.password)
-            .then(isMatch => {
-                if(isMatch) {
-                    // 회원 비밀번호가 일치할 때
-                    // JWT PAYLOAD 생성
-                    const payload = {
-                        id: user.id,
-                        name: user.name
-                    };
-                    tokenmaker(payload,user,res);
-                } else {
-                    errors.password = "패스워드가 일치하지 않습니다.";
-                    return res.status(400).json(errors);
-                }
-            });
-    }
-
-    tokenmaker(payload,user,res)
-    {
-        const token = jwt.sign(payload, keys.secretOrKey, { expiresIn: '7d' });
-
-        user.token = token;
-        user.save((error, user) => {
-            if (error) {
-                return res.status(400).json({ error: "something wrong"});
+    // email로 회원 찾기
+    User.findOne({email})
+        .then(user => {
+            if(!user){
+                errors.email = "해당하는 회원이 존재하지 않습니다.";
+                return res.status(400).json(errors);
             }
-            console.log("여기들어왓는데 왜 쿠키가 안만들어짐?");
-            return res.cookie("x_auth", user.token, {
-                // JWT 토큰 생성
-                // 7일 동안 유효
-                maxAge: 1000*60*60*24*7,
-                httpOnly: true,
-            })
-                .status(200)
-                .json({ loginSuccess: true, userId: user.id, token: user.token});
+            bcryptchecker(password, user, res);
         });
-    }
 }
 
-module.exports.login = login_Manager;
+function bcryptchecker(password, user, res)
+{
+    bcrypt.compare(password, user.password)
+        .then(isMatch => {
+            if(isMatch) {
+                // 회원 비밀번호가 일치할 때
+                // JWT PAYLOAD 생성
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                tokenmaker(payload,user,res);
+            } else {
+                errors.password = "패스워드가 일치하지 않습니다.";
+                return res.status(400).json(errors);
+            }
+        });
+}
+
+function tokenmaker(payload,user,res)
+{
+    const token = jwt.sign(payload, keys.secretOrKey, { expiresIn: '7d' });
+
+    user.token = token;
+    user.save((error, user) => {
+        if (error) {
+            return res.status(400).json({ error: "something wrong"});
+        }
+        console.log("여기들어왓는데 왜 쿠키가 안만들어짐?");
+        return res.cookie("x_auth", user.token, {
+            // JWT 토큰 생성
+            // 7일 동안 유효
+            maxAge: 1000*60*60*24*7,
+            httpOnly: true,
+        })
+            .status(200)
+            .json({ loginSuccess: true, userId: user.id, token: user.token});
+    });
+}
+
+module.exports.login = login;
