0fc969509f186e3836a20adc613e5c67d464d454
Lixmna <kang7145471@naver.com>
2021-01-30 19:25:22 +0900

    log

--- routes/Friend_router/newFriend.js
+++ routes/Friend_router/newFriend.js
@@ -6,7 +6,7 @@ function newFriend(req, res)
 
     const Friend_Email = req.body.friend_email;
     const User_id = req.user.id;
-    
+    console.log(Friend_Email);
     User.findOne({Friend_Email})
         .then(user => {
             if(!user){
