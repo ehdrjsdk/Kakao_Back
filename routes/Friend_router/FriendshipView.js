68ffaad3cb9488dab5fcd2575574645fb4af3dbd
Lixmna <kang7145471@naver.com>
2021-01-31 20:11:36 +0900

    var

--- routes/Friend_router/FriendshipView.js
+++ routes/Friend_router/FriendshipView.js
@@ -3,12 +3,12 @@ const User = require("../../models/User");
 function FriendshipView(req, res)
 {
     const Friend_id = req.user.friendship;
-    const Merge_Friend_Name = [];
+    var Merge_Friend_Name;
 
     console.log(Friend_id.length);
     for(var i=0;i<Friend_id.length;i++) {
         User.findOne({ _id : Friend_id[i] })
-        .then((user,Merge_Friend_Name) => {
+        .then((user, Merge_Friend_Name) => {
             if(!user){
                 console.log(user);
                 errors = "해당하는 회원이 존재하지 않습니다.";
