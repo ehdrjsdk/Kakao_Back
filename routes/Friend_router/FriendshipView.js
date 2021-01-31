b6125024a88f265d41656abd2f69a24ffa0df614
Lixmna <kang7145471@naver.com>
2021-01-31 20:31:16 +0900

    ??

--- routes/Friend_router/FriendshipView.js
+++ routes/Friend_router/FriendshipView.js
@@ -7,18 +7,18 @@ function FriendshipView(req, res)
 
     console.log(Friend_id.length);
     for(var i=0;i<Friend_id.length;i++) {
-        User.findOne({ _id : Friend_id[i] })
-        .then(user => {
-            if(!user){
-                console.log(user);
-                errors = "해당하는 회원이 존재하지 않습니다.";
-                return res.status(400).json(errors);
-            }
-            Merge_Friend_Name.push(user.name);
-            console.log(i,Friend_id);
-        });
+
     }
-    
+    User.findOne({ _id : Friend_id[i] })
+    .then(user => {
+        if(!user){
+            console.log(user);
+            errors = "해당하는 회원이 존재하지 않습니다.";
+            return res.status(400).json(errors);
+        }
+        Merge_Friend_Name.push(user.name);
+        console.log(Merge_Friend_Name);
+    });
     console.log(Merge_Friend_Name);
 
     return res.status(200).json({name : Merge_Friend_Name});    
