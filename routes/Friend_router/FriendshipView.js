177a49af5e8e5bdb10c7d5c39a22f736ab4f0f93
Lixmna <kang7145471@naver.com>
2021-01-31 18:27:20 +0900

    일단해봐

--- routes/Friend_router/FriendshipView.js
+++ routes/Friend_router/FriendshipView.js
@@ -1,9 +1,23 @@
+const User = require("../../models/User");
 
 function FriendshipView(req, res)
 {
-    res.status(200).json({
-        friendship : req.user.friendship,
-    });
+    const Friend_id = req.body.frendship;
+    const Merge_Friend_Name;
+
+    User.findOne({_id : Friend_id})
+        .then(user => {
+            if(!user){
+                console.log(user);
+                errors = "해당하는 회원이 존재하지 않습니다.";
+                return res.status(400).json(errors);
+            }
+            Merge_Friend_Name.push(user.name);
+            console.log(Merge_Friend_Name);
+        })
+    
+    return res.status(200).json({name : Merge_Friend_Name});
 }
+
 
 module.exports.FriendshipView = FriendshipView;
