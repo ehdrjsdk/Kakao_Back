24b0040443b466699e4c94ec7b31237f3504210d
Lixmna <kang7145471@naver.com>
2021-01-31 20:04:41 +0900

    newArray

--- routes/Friend_router/FriendshipView.js
+++ routes/Friend_router/FriendshipView.js
@@ -3,7 +3,7 @@ const User = require("../../models/User");
 function FriendshipView(req, res)
 {
     const Friend_id = req.user.friendship;
-    const Merge_Friend_Name = [];
+    const Merge_Friend_Name = newArray();
 
     console.log(Friend_id.length);
     for(var i=0;i<Friend_id.length;i++) {
@@ -15,12 +15,12 @@ function FriendshipView(req, res)
                 return res.status(400).json(errors);
             }
             Merge_Friend_Name.push(user.name);
-            
-            return res.status(200).json({name : Merge_Friend_Name});
         });
     }
     
     console.log(Merge_Friend_Name);
+
+    return res.status(200).json({name : Merge_Friend_Name});    
 }
 
 
