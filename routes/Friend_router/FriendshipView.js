88d1d614f5b3105ec95b4b4781f6e7ab9eaa0cb8
Lixmna <kang7145471@naver.com>
2021-01-31 18:37:21 +0900

    입력이아니잖아

--- routes/Friend_router/FriendshipView.js
+++ routes/Friend_router/FriendshipView.js
@@ -2,11 +2,12 @@ const User = require("../../models/User");
 
 function FriendshipView(req, res)
 {
-    const Friend_id = req.body.friendship;
+    const Friend_id = req.user.friendship;
     const Merge_Friend_Name = [];
 
     console.log(Friend_id);
-    User.findOne({_id : Friend_id})
+
+    User.findOne({ _id : Friend_id })
         .then(user => {
             if(!user){
                 console.log(user);
