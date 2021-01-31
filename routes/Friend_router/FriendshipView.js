4ee726d24cbfa17650d0dd91a427e365fac43c6f
Lixmna <kang7145471@naver.com>
2021-01-31 20:51:31 +0900

    해봣는데 또함

--- routes/Friend_router/FriendshipView.js
+++ routes/Friend_router/FriendshipView.js
@@ -8,6 +8,7 @@ function FriendshipView(req, res)
     console.log(Friend_id.length);
     for(var i=0;i<Friend_id.length;i++) {
 
+        console.log(i);
         User.findOne({ _id : Friend_id[i] })
         .then(user => {
             if(!user){
@@ -15,8 +16,7 @@ function FriendshipView(req, res)
                 errors = "해당하는 회원이 존재하지 않습니다.";
                 return res.status(400).json(errors);
             }
-            Merge_Friend_Name.push(user.name);        
-            console.log(Friend_id[i]);
+            Merge_Friend_Name.push(user.name);
             console.log(i,Merge_Friend_Name);
 
             if(i==Merge_Friend_Name.length)
