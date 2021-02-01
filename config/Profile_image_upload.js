d35c67f7d759a2acd92ed3aad2777996bdcd36a2
Lixmna <kang7145471@naver.com>
2021-02-02 01:17:42 +0900

    되나?

--- config/Profile_image_upload.js
+++ config/Profile_image_upload.js
@@ -8,14 +8,14 @@ const Profile_image_upload = multer({
   storage: multer.diskStorage({
     destination: function (req, file, cb) {
         console.log('2');
-        cb(null, __dirname + '../../../Profile_image/');
+        cb(null, __dirname + '/../Profile_image/');
     },
     filename: function (req, file, cb) {
         console.log('1');
       cb(null, new Date().valueOf() + path.extname(file.originalname));
     }
   }),
-  
+
 });
 
 module.exports.Profile_image_upload = Profile_image_upload;
