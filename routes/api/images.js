cdcbc23bad0eb6e036bab597462a37c2d5d2862b
Lixmna <kang7145471@naver.com>
2021-02-02 06:33:58 +0900

    post

--- routes/api/images.js
+++ routes/api/images.js
@@ -3,6 +3,6 @@ var router = express.Router();
 
 const Profile_image_opener = require('../image_controller/Profile_image_opener');
 
-router.get('/Profile_image_opener', Profile_image_opener.Profile_image_opener);
+router.post('/Profile_image_opener', Profile_image_opener.Profile_image_opener);
 
 module.exports = router;
