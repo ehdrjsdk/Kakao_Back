320dcd2f440f2bec3f27698690fbaa4f595a9cc0
Lixmna <kang7145471@naver.com>
2021-02-02 06:00:17 +0900

    schema

---
+++ models/Profile_image_Scheama.js
@@ -0,0 +1,44 @@
+/** @module Profile_image_Schema */
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/**
+ * <pre>
+ * email, password, name, token의 type과 존재한다는것을 어디서나 알수있게 해주는
+ * Schema
+ * </pre>
+ * @constant Profile_image_Schema
+ */
+
+const Profile_image_Schema = new Schema({
+    fieldname: {
+        type: String,
+        required: true
+    },
+    originalname: {
+        type: String,
+        required: true
+    },
+    encoding : {
+        type: String,
+        required: true
+    },
+    mimetype : {
+        type: String,
+        required: true
+    },
+    filename : {
+        type: Array,
+        required: true
+    },
+    path :  {
+        type : String,
+        required: true
+    },
+    size :  {
+        type : Number,
+        required:  true
+    },
+});
+
+module.exports = Profile_image_Schema = mongoose.model('Profile_image', Profile_image_Schema);
--- routes/image_controller/Profile_image.js
+++ routes/image_controller/Profile_image.js
@@ -1,7 +1,20 @@
+const Profile_image_Schema = require('../../models/Profile_image_Scheama');
 
 function Profile_image(req, res)
 {
-    console.log(req.user.name);
+    const newProfile_image = new Profile_image_Schema({
+        fileldname: req.file.fileldname,
+        originalname: req.file.originalname,
+        encoding: req.file.encoding,
+        mimetype: req.file.mimetype,
+        destination: req.file.destination,
+        filename: req.file.filename,
+        path: req.file.path,
+        size: req.file.size,
+    });
+
+    newProfile_image.save();
+    
     console.log(req.file);
 }
 
