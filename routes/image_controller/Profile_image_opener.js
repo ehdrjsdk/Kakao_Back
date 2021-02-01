44b41b286af8cfdcbb23134384390369f4db8e5e
Lixmna <kang7145471@naver.com>
2021-02-02 06:39:00 +0900

    path .resolve

--- routes/image_controller/Profile_image_opener.js
+++ routes/image_controller/Profile_image_opener.js
@@ -1,7 +1,9 @@
+var path = require('path');
+
 function Profile_image_opener(req,res)
 {    
     console.log(__dirname + '/../../Profile_image/'+ req.body.filename)
-    res.status(200).sendFile(__dirname + '/../../Profile_image/'+ req.body.filename);
+    res.status(200).sendFile(path.resolve('Profile_image/'+ req.body.filename));
 }
 
 module.exports.Profile_image_opener = Profile_image_opener;
