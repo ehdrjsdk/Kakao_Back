fc863b81d5cbd4b135db4e312fd60980fbc91aa4
Lixmna <kang7145471@naver.com>
2021-02-11 16:51:21 +0900

    get

--- routes/login_router/login.js
+++ routes/login_router/login.js
@@ -111,7 +111,6 @@ function tokenmaker(payload,user,res)
         }
         console.log("여기들어왓는데 왜 쿠키가 안만들어짐?");
         return res.cookie("x_auth", user.token, {
-            Secure: true,
             maxAge: 1000*60*60*24*7,
             httpOnly: true,
             sameSite: "Lax",
